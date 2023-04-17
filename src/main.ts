8c990395d672bb7dc605127f02eecef671501998
caijianxing <1760603940@qq.com>
2023-04-17 16:26:59 +0800

    测试ui库

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import {
-    // create naive ui
-    create,
-    // component
-    NButton,
-    NForm,
-    NFormItem,
-    NInput,
-    NUpload,
-    NImage,
-    NUploadDragger,
-    NIcon,
-    NCheckbox,
-    NCheckboxGroup,
-    NSpace
-} from 'naive-ui'
-const naive = create({
-    components: [NButton, NForm, NFormItem, NInput, NUpload, NImage, NUploadDragger, NIcon, NCheckbox,
-        NCheckboxGroup, NSpace]
-})
+// import {
+//     // create naive ui
+//     create,
+//     // component
+//     NButton,
+//     NForm,
+//     NFormItem,
+//     NInput,
+//     NUpload,
+//     NImage,
+//     NUploadDragger,
+//     NIcon,
+//     NCheckbox,
+//     NCheckboxGroup,
+//     NSpace
+// } from 'naive-ui'
+// const naive = create({
+//     components: [NButton, NForm, NFormItem, NInput, NUpload, NImage, NUploadDragger, NIcon, NCheckbox,
+//         NCheckboxGroup, NSpace]
+// })
 
 import App from './App.vue'
 import router from './router'
@@ -28,7 +28,7 @@ import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(naive) //组件库
+// app.use(naive) //组件库
 app.use(createPinia())
 app.use(router)
 
