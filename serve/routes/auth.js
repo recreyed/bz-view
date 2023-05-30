c5832a9293753fed4e8b276fe3892336df54a4cb
caijianxing <1760603940@qq.com>
2023-05-30 12:59:45 +0800

    删除express的serve文件夹

--- serve/routes/auth.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// process.cwd() 表示命令执行的文件路径
-const { getAuth } = require(process.cwd() + '/controller/Auth')
-router.post('/', getAuth)
-
-module.exports = router;
--- serve/routes/upload.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// process.cwd() 表示命令执行的文件路径
-const { getUpload,upload } = require(process.cwd() + '/controller/Upload')
-router.post('/',upload.single('file_'), getUpload)
-
-module.exports = router;
